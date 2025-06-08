ce7af0cba1e5a0782a2f56381a9fcc93dda2a442
Clemenz88 <clemben@gmx.de>
2025-06-08 21:42:49 +0200

    update

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,9 +1,1 @@
-
-from PIL import Image
-
-def load_image(image_path):
-    return Image.open(image_path)
-
-def detect_hand_and_food_area(image):
-    # Dummy placeholder
-    return image
+# Funktioner til billedbehandling
--- utils/matcher.py
+++ utils/matcher.py
@@ -1,4 +1,1 @@
-
-def match_food_label(label):
-    # Dummy placeholder for synonym or fuzzy match logic
-    return label
+# Fuzzy matching funktioner
